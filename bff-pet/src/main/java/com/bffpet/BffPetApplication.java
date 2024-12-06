3474354827d05133ad527a8c15410693c0d7578e
gui.miranda <hahnmiranda@gmail.com>
2024-12-05 21:54:01 -0300

    first commit

---
+++ bff-pet/src/main/java/com/bffpet/BffPetApplication.java
@@ -0,0 +1,13 @@
+package com.bffpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffPetApplication.class, args);
+	}
+
+}
---
+++ bff-pet/src/test/java/com/bffpet/BffPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bffpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
